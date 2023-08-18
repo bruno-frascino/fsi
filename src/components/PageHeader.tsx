a8c792de90d8cf57a8404a5a8d4c349dd446e27f
Bruno Frascino <bfrascino80@gmail.com>
2023-08-18 23:37:05 +1000

    feat: Page header

---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,15 @@
+import './PageHeader.css';
+
+interface PageHeaderProps {
+
+}
+
+const PageHeader = (props: PageHeaderProps) => {
+  return (
+    <header className="App-header">
+      <h1>Integração Farmer Shop</h1>
+    </header>
+  );
+}
+
+export default PageHeader;
